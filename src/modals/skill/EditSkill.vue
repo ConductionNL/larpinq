60243cbb64a4c7571719fdf8abb6d7ac8b97a764
Thijn <Thijn@conduction.nl>
2024-09-11 14:00:56 +0200

    added effects select to skill and fixed effects tab on skills detail

--- src/modals/skill/EditSkill.vue
+++ src/modals/skill/EditSkill.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { skillStore, navigationStore } from '../../store/store.js'
+import { skillStore, navigationStore, effectStore } from '../../store/store.js'
 </script>
 
 <template>
@@ -7,7 +7,6 @@ import { skillStore, navigationStore } from '../../store/store.js'
 		name="Vaardigheid"
 		size="normal"
 		:can-close="false">
-
 		<NcNoteCard v-if="success" type="success">
 			<p>Vaardigheid succesvol aangepast</p>
 		</NcNoteCard>
@@ -19,36 +18,45 @@ import { skillStore, navigationStore } from '../../store/store.js'
 			<NcTextField :disabled="loading"
 				label="Name *"
 				required
-				:value.sync="skillStore.skillItem.name" />
+				:value.sync="skillItem.name" />
 			<NcTextArea :disabled="loading"
 				label="Description"
 				type="textarea"
-				:value.sync="skillStore.skillItem.description" />
-			<NcTextField :disabled="loading"
-				label="Effect"
-				:value.sync="skillStore.skillItem.effect" />
+				:value.sync="skillItem.description" />
 			<NcTextField :disabled="loading"
 				label="Required Score"
 				type="number"
-				:value.sync="skillStore.skillItem.requiredScore" />
+				:value.sync="skillItem.requiredScore" />
+			<NcSelect v-bind="effects"
+				v-model="effects.value"
+				input-label="Effects"
+				:loading="effectsLoading"
+				:disabled="loading" />
 		</div>
 
 		<template #actions>
-			<NcButton @click="navigationStore.setModal(false)">
-				<template #icon><Cancel :size="20" /></template>
+			<NcButton @click="closeModal">
+				<template #icon>
+					<Cancel :size="20" />
+				</template>
 				{{ success ? 'Sluiten' : 'Annuleer' }}
 			</NcButton>
 			<NcButton @click="openLink('https://conduction.gitbook.io/opencatalogi-nextcloud/gebruikers/publicaties', '_blank')">
-				<template #icon><Help :size="20" /></template>
+				<template #icon>
+					<Help :size="20" />
+				</template>
 				Help
 			</NcButton>
-			<NcButton v-if="!success" :disabled="loading" type="primary" @click="editSkill()">
+			<NcButton v-if="!success"
+				:disabled="loading"
+				type="primary"
+				@click="editSkill()">
 				<template #icon>
 					<NcLoadingIcon v-if="loading" :size="20" />
-					<ContentSaveOutline v-if="!loading && skillStore.skillItem.id" :size="20" />
-					<Plus v-if="!loading && !skillStore.skillItem.id" :size="20" />
+					<ContentSaveOutline v-if="!loading && skillStore.skillItem?.id" :size="20" />
+					<Plus v-if="!loading && !skillStore.skillItem?.id" :size="20" />
 				</template>
-				{{ skillStore.skillItem.id ? 'Opslaan' : 'Aanmaken' }}
+				{{ skillStore.skillItem?.id ? 'Opslaan' : 'Aanmaken' }}
 			</NcButton>
 		</template>
 	</NcDialog>
@@ -56,7 +64,13 @@ import { skillStore, navigationStore } from '../../store/store.js'
 
 <script>
 import {
-	NcButton, NcDialog, NcTextField, NcTextArea, NcLoadingIcon, NcNoteCard
+	NcButton,
+	NcDialog,
+	NcTextField,
+	NcTextArea,
+	NcLoadingIcon,
+	NcNoteCard,
+	NcSelect,
 } from '@nextcloud/vue'
 import ContentSaveOutline from 'vue-material-design-icons/ContentSaveOutline.vue'
 import Cancel from 'vue-material-design-icons/Cancel.vue'
@@ -66,39 +80,111 @@ import Help from 'vue-material-design-icons/Help.vue'
 export default {
 	name: 'EditSkill',
 	components: {
-		NcDialog, NcTextField, NcTextArea, NcButton, NcLoadingIcon, NcNoteCard,
-		ContentSaveOutline, Cancel, Plus, Help,
+		NcDialog,
+		NcTextField,
+		NcTextArea,
+		NcSelect,
+		NcButton,
+		NcLoadingIcon,
+		NcNoteCard,
+		ContentSaveOutline,
+		Cancel,
+		Plus,
+		Help,
 	},
 	data() {
 		return {
+			skillItem: {
+				name: '',
+				description: '',
+				requiredScore: '',
+			},
+			effects: {},
+			effectsLoading: false,
 			success: false,
 			loading: false,
 			error: false,
+			hasUpdated: false,
+		}
+	},
+	updated() {
+		if (navigationStore.modal === 'editSkill' && !this.hasUpdated) {
+			if (skillStore.skillItem.id) {
+				this.skillItem = {
+					...skillStore.skillItem,
+					name: skillStore.skillItem.name || '',
+					description: skillStore.skillItem.description || '',
+					requiredScore: skillStore.skillItem.requiredScore || '',
+				}
+			}
+			this.fetchEffects()
+			this.hasUpdated = true
 		}
 	},
 	methods: {
+		closeModal() {
+			navigationStore.setModal(false)
+			this.success = false
+			this.loading = false
+			this.error = false
+			this.hasUpdated = false
+			this.skillItem = {
+				name: '',
+				description: '',
+				requiredScore: '',
+			}
+		},
+		fetchEffects() {
+			this.effectsLoading = true
+
+			effectStore.refreshEffectList()
+				.then(() => {
+					const activatedEffects = skillStore.skillItem.id // if modal is an edit modal
+						? effectStore.effectList.filter((effect) => { // filter through the list of effects
+							return (skillStore.skillItem.effects || [])
+								.map(String) // ensure all the effect id's in the skill are a string (this does not change the resulting data type)
+								.includes(effect.id.toString()) // check if the current effect in the filter exists on the skill's effects
+						})
+						: null
+
+					this.effects = {
+						multiple: true,
+						closeOnSelect: false,
+						options: effectStore.effectList.map((effect) => ({
+							id: effect.id,
+							label: effect.name,
+						})),
+						value: activatedEffects
+							? activatedEffects.map((effect) => ({
+								id: effect.id,
+							    label: effect.name,
+							}))
+							: null,
+					}
+
+					this.effectsLoading = false
+				})
+		},
 		async editSkill() {
 			this.loading = true
 			try {
-				await skillStore.saveSkill()
+				await skillStore.saveSkill({
+					...this.skillItem,
+					effects: this.effects.value.map((effect) => effect.id),
+				})
 				this.success = true
 				this.loading = false
-				setTimeout(() => {
-					this.success = false
-					this.loading = false
-					this.error = false
-					navigationStore.setModal(false)
-				}, 2000)
+				setTimeout(this.closeModal, 2000)
 			} catch (error) {
 				this.loading = false
 				this.success = false
-				
+
 				this.error = error.message || 'An error occurred while saving the skill'
 			}
 		},
 		openLink(url, target) {
 			window.open(url, target)
-		}
+		},
 	},
 }
 </script>
