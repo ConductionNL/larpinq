67417d1e67d8c4275e7a8b23d588337eef61f0f9
Ruben van der Linde <rubenvdlinde@gmail.com>
2024-12-16 22:23:24 +0100

    Remove stat id from efects

--- src/entities/effect/effect.types.ts
+++ src/entities/effect/effect.types.ts
@@ -2,8 +2,8 @@ export type TEffect = {
     id?: string
     name: string
     description?: string
-    stat?: string // UUID of the stat
     modifier?: number
     modification: 'positive' | 'negative'
     cumulative: 'cumulative' | 'non-cumulative'
+	abilities: string[]
 }
